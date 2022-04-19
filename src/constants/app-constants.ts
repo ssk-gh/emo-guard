bd2311b6fde7ff94b8479789490b4417805975cd
ssk-gh <78728203+ssk-gh@users.noreply.github.com>
2022-04-19 04:49:03 +0000

    Add hiding mode to each css selector item
    - Add mode to hide elements matching keywords
    - Add mode to hide text matching keywords

--- src/constants/app-constants.ts
+++ src/constants/app-constants.ts
@@ -2,4 +2,6 @@ export class AppConstants {
     static readonly AppName = 'EmoGuard';
     static readonly ThisSite = 'this site';
     static readonly AllSites = 'all sites';
+    static readonly ElementHideMode = 0;
+    static readonly TextHideMode = 1;
 }
